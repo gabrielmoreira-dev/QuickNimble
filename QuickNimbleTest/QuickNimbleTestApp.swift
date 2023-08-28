79cfbc45529c0965730a6045454fa39df53c50a6
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-08-27 21:31:14 -0300

    Initial Commit

---
+++ QuickNimbleTest/QuickNimbleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNimbleTestApp.swift
+//  QuickNimbleTest
+//
+//  Created by Gabriel Moreira on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickNimbleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
